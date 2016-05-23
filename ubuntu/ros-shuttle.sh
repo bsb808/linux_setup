9eeb2140e55ea642b3bd50cfac64b71ce5d97038
Brian Bingham <briansbingham@gmail.com>
2016-05-23 12:52:35 -0700

    tweak

--- ubuntu/ros-shuttle.sh
+++ ubuntu/ros-shuttle.sh
@@ -4,6 +4,7 @@ git clone https://github.com/bsb808/nre_p3at.git
 git clone https://github.com/bsb808/nre_joy.git
 git clone https://github.com/bsb808/nre_common.git
 git clone https://github.com/amor-ros-pkg/rosaria.git
+git clone https://github.com/allenh1/p2os.git
 # Re-setup your ros environment so that the system knows about our new package
 source ~/catkin_ws/devel/setup.bash
 
@@ -14,4 +15,4 @@ cd ~/catkin_ws
 # Compile source
 catkin_make
 # Re-setup your ros environment so that the system knows about our new package
-source ~/catkin_ws/devel/setup.bash
+source ~/catkin_ws/devel/setup.bash3
