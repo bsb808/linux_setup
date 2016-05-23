af78687f258af9c0871087880d3199b404522187
bsb808 <briansbingham@gmail.com>
2016-05-23 05:35:50 -0700

    increment

--- ubuntu/ros-shuttle.sh
+++ ubuntu/ros-shuttle.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 cd ~/catkin_ws/src
 git clone https://github.com/bsb808/nre_p3at.git
+git clone https://github.com/bsb808/nre_joy.git
+git clone https://github.com/bsb808/nre_common.git
+git clone https://github.com/amor-ros-pkg/rosaria.git
 # Re-setup your ros environment so that the system knows about our new package
 source ~/catkin_ws/devel/setup.bash
-# If everything is working, this should put you in the package directory
-roscd nre_p3at
+
+rosdep update
+rosdep install rosaria
+# Change directory
+cd ~/catkin_ws
+# Compile source
+catkin_make
+# Re-setup your ros environment so that the system knows about our new package
+source ~/catkin_ws/devel/setup.bash
