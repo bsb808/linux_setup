6762c966dcbdc9e8d168961275df9d65d76e7a9f
Bingham, Brian S <briansbingham@gmail.com>
2016-12-09 14:49:15 -0800

    commented in a couple things for ros install

--- ubuntu/aptget-ros-indigo.sh
+++ ubuntu/aptget-ros-indigo.sh
@@ -8,9 +8,11 @@
 # Following installation instructions from here...
 # http://wiki.ros.org/indigo/Installation/Ubuntu
 
-#sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 #apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 apt-get update
 
