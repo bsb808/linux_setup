db921239d622692e2cd8cd99341be204b5e6f5bd
Bingham, Brian S <bsbingha@nps.edu>
2017-04-20 16:15:48 -0700

    remote admin

--- mrc/carpc_login.sh
+++ mrc/carpc_login.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-for N in $(seq 0 4)
+SCRIPT="source ~/.bashrc; rosnode list;"
+SCRIPT="rosnode list; pwd"
+
+for N in $(seq 0 7)
 do
     IP="192.168.11.6${N}"
+    echo "--------"
     echo ${IP}
-    gnome-terminal -e "bash -c \"ssh frl@${IP}; exec bash\""
+
+    # Open a new window on each machine
+    #gnome-terminal -e "bash -c \"ssh frl@${IP}; exec bash; rosnode list\""
+    #ssh frl@${IP} -t 'bash -l -c \"${SCRIPT}\"'
+    #ssh frl@${IP} -t "exec bash -i -c \"source ~/.bashrc; rosnode list\""
+    ssh frl@${IP} -t "exec bash -i -c \"rosnode list\""
+    # Install ssh keys
+    #ssh-copy-id -i ~/.ssh/id_rsa.pub frl@${IP}
+    #ssh frl@${IP}
 done
