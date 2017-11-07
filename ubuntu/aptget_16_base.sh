f49ececce6ff0c6a2d94f35e1550e63561064a04
bsb <briansbigham@gmail.com>
2017-11-07 15:17:44 -0800

    adjusting base for 16.04

--- ubuntu/aptget_16_base.sh
+++ ubuntu/aptget_16_base.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-# Installation script for Ubuntu 14.04
+# Installation script for Ubuntu 16.04
 apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
 
 apt-get -y install \
     cifs-utils \
-    emacs \
     git \
     gksu \
     gtkterm \
     minicom \
-    nautilus-actions \
     gnome-terminal \
     ntp \
     python-numpy python-scipy python-matplotlib ipython \
     ssh \
     subversion \
     openssh-server \
-    synaptic \
     vim \
     zip \
-    unzip \
+    unzip
 
 
 # For mounting FAT drives
