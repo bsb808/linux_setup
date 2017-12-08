60524242130d494118900567da12657dfbba9e38
Bingham, Brian S <bsbingha@nps.edu>
2017-12-08 08:33:58 -0800

    towards robotx

---
+++ robotx_setup/set_firefox_default.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DIR="$(ls -d ~/.mozilla/firefox/*.default)"
+cp user.js $DIR
+
---
+++ robotx_setup/user.js
@@ -0,0 +1,2 @@
+user_pref("browser.startup.homepage", "https://wiki.nps.edu/display/ROSSC/Robotics+Operating+System+Short-Course+Home");
+
