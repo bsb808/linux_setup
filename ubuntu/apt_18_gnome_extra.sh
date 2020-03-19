25211ab1e2784dea149649a77dc5b4ed00200b52
Brian Bingham <briansbingham@gmail.com>
2020-03-19 09:15:23 -0700

    increments

--- ubuntu/apt_18_gnome_extra.sh
+++ ubuntu/apt_18_gnome_extra.sh
@@ -19,6 +19,7 @@ apt install \
     keepassx \
     rar \
     p7zip-rar p7zip-full unace unrar sharutils \
+    texlive-full jabref latexmk \
     virtualbox \
     xournal \
     chromium-browser \
