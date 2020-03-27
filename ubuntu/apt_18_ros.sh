fe3f9482cd224c9845a697e347d67224d6b766ff
Brian Bingham <briansbingham@gmail.com>
2020-03-27 15:10:24 -0700

    update for 18

--- ubuntu/apt_18_ros.sh
+++ ubuntu/apt_18_ros.sh
@@ -2,9 +2,10 @@
 
 # accept packages from ROS
 echo Accept packages from ROS...
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-sudo -E apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 apt update
 
