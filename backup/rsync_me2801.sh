997bb22cb054b224c8d14d78aaaa3c600319ff04
Brian Bingham <briansbingham@gmail.com>
2020-10-15 11:45:03 -0700

    exclude

--- backup/rsync_me2801.sh
+++ backup/rsync_me2801.sh
@@ -3,5 +3,7 @@
 
 EXTER=/media/bsb/MOE-Bingham/iwi/bsb/Classes/ME2801/
 LOCAL=${HOME}/Classes/ME2801/
+DR='--dry-run'
+DR=''
 
-rsync -av --dry-run ${LOCAL} ${EXTER}
+rsync -av ${DR} --exclude-from='exclude_me2801.txt' ${EXTER} ${LOCAL}
