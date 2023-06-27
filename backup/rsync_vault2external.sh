f8361ddb76ec60ab22e8c752fdb0e42fbed3c6ca
Brian Bingham <briansbingham@gmail.com>
2023-06-26 21:34:38 -0700

    routine backups

--- backup/rsync_vault2external.sh
+++ backup/rsync_vault2external.sh
@@ -4,8 +4,9 @@ SRC_ROOT="/data/Vault"
 # External
 #DEST="/media/bsb/BareExt4Alpha/"
 #DEST="/media/bsb/Passport2TB/Vault/"
-DEST="/media/bsb/FRL-Backup/Vault/"
-declare -a SRC_DIRS=("Archive" "Photos" "Movies" )
+#DEST="/media/bsb/FRL-Backup/Vault/"
+DEST="/media/bsb/PassportSilver/Vault/"
+declare -a SRC_DIRS=("Archive" "Photos" "Movies" "Videos")
 #declare -a SRC_DIRS=("Archive")
 for SRC_DIR in "${SRC_DIRS[@]}"
 do
